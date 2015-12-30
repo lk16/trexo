bf22b59fb82f868192ddb9746af63fb56afcfb44
Luuk Verweij <luuk_verweij@msn.com>
2015-12-30 16:31:18 +0100

    first working ui! removed gtk_window_set_default_size()

--- src/clickable_image.c
+++ src/clickable_image.c
@@ -1,10 +1,10 @@
 #include "clickable_image.h"
 
 void clickable_image_init(
-  struct clickable_image* ci, 
-  const char* file,
-  int index,
-  struct trexo_game_config* gc
+    struct clickable_image* ci, 
+    const char* file,
+    int index,
+    struct trexo_game_config* gc
 ){
     ci->gc = gc;
     ci->index = index;  
@@ -14,20 +14,20 @@ void clickable_image_init(
     g_signal_connect(ci->ebox,"button-press-event",
         G_CALLBACK(clickable_image_on_click),
         ci
-  );
+    );
 }
 
 void game_config_process_click(struct trexo_game_config*,int,int);
 
 void clickable_image_on_click(
-  GtkWidget* widget,
-  GdkEvent* event,
-  struct clickable_image* ci
+    GtkWidget *widget,
+    GdkEvent *event,
+    struct clickable_image* ci
 ){
-  (void)widget;
-  if(event->button.type==GDK_BUTTON_PRESS && 
-    (event->button.button==LEFT_BUTTON || event->button.button==RIGHT_BUTTON)
-  ){
-    trexo_game_config_process_click(ci->gc,ci->index,event->button.button);
-  }
+    (void)widget;
+    if(event->button.type==GDK_BUTTON_PRESS && 
+        (event->button.button==LEFT_BUTTON || event->button.button==RIGHT_BUTTON)
+    ){
+        trexo_game_config_process_click(ci->gc,ci->index,event->button.button);
+    }
 }
--- src/main.c
+++ src/main.c
@@ -1,33 +1,23 @@
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <gtk/gtk.h>
+#include <glib-object.h>
+#include <time.h>
+
+#include "main_window.h"
 #include "board.h"
 
-#include <stdio.h>
-
-int main();
-
-int main(){
-
-	struct trexo_board board;
-	trexo_board_init(&board);
-
-	struct trexo_board children[TREXO_MAX_CHILDREN];
-	struct trexo_board *child_end;
 
 
-	trexo_board_get_children(&board,children,&child_end);
-
-	board = children[0];
-
-	trexo_board_get_children(&board,children,&child_end);
-
-	struct trexo_board *child_iter = children;
-
-	while(child_iter != child_end){
-		trexo_board_print(child_iter);
-		++child_iter;
-	}
-
-	printf("There are %ld children.\n",child_end - children);
-
-
-	return 0;
+int main(int argc,char** argv){
+    srand(time(NULL));
+    struct trexo_main_window window;
+    gtk_init(&argc,&argv);
+    trexo_main_window_init(&window);  
+    //arg_parse(argc,(const char**)argv,&mw.config);
+    //g_timeout_add(100,(GSourceFunc)trexo_game_config_timeout,&window.config);
+    gtk_main();
+    return 0;  
 }
