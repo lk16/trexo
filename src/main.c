4ab4e33c0580e1885fce004cdb19203abe83b137
Luuk Verweij <luuk_verweij@msn.com>
2016-01-02 19:25:41 +0100

    removed test code

--- src/main.c
+++ src/main.c
@@ -15,24 +15,11 @@ int main(
 	int argc,
 	char **argv
 ){
-    (void)argc;
-    (void)argv;
-
-    struct trexo_board board,child;
-    trexo_board_init(&board);
-    struct trexo_child_generator gen;
-    trexo_child_generator_init(&gen);
-    while(trexo_child_generator_next(&gen,&board,&child)){
-        trexo_board_print(&child);
-    }
-
-
-
-	/*srand(time(NULL));
+	srand(time(NULL));
     struct trexo_main_window window;
     gtk_init(&argc,&argv);
     trexo_main_window_init(&window);  
     //g_timeout_add(100,(GSourceFunc)trexo_game_config_timeout,&window.config);
     gtk_main();
-    return 0;*/  
+    return 0;  
 }
