8ee40ea03743393233c83422abe438df3139df33
Luuk Verweij <luuk_verweij@msn.com>
2016-01-03 17:01:53 +0100

    working on bitset macros

--- src/bitset.h
+++ src/bitset.h
@@ -1,14 +1,17 @@
 #pragma once
 
+#include <assert.h>
 #include <stdint.h>
+#include <string.h>
+#include <stdio.h>
 
-#include "macros.h"
+#include "const.h"
 
 const uint64_t uint64_set[64];
 const uint64_t uint64_reset[64];
-const uint64_t uint64_before[64];
-const uint64_t uint64_after[64];
 const int uint64_counts[65536];
+
+
 
 // returns 64 if b==0ull, returns most significant bit otherwise
 int uint64_first_index(uint64_t b);
@@ -16,23 +19,53 @@ int uint64_first_index(uint64_t b);
 // returns index of the only set bit in b
 int uint64_only_bit_index(uint64_t b);
 
-// returns 64 if b==0ull, returns most significant bit otherwise
-int uint64_last_index(uint64_t b);
-
 uint64_t uint64_first_bit(uint64_t b);
 
 // returns number of set bits
 int uint64_count(uint64_t b);
 
 
+#define BITSET_DECLARE(SIZE)\
+struct bits##SIZE{\
+    uint64_t data[1+((SIZE-1)/64)];\
+};\
+void bits##SIZE##_init(struct bits##SIZE *bits);\
+void bits##SIZE##_clear(struct bits##SIZE *bits);\
+void bits##SIZE##_copy(const struct bits##SIZE *bits,struct bits##SIZE *copy);\
+void bits##SIZE##_print(const struct bits##SIZE *bits);\
+bool bits##SIZE##_test(const struct bits##SIZE *bits,unsigned offset);
+
+
+#define BITSET_DEFINE(SIZE)\
+void bits##SIZE##_init(struct bits##SIZE *bits){\
+    bits##SIZE##_clear(bits);\
+}\
+void bits##SIZE##_clear(struct bits##SIZE *bits){\
+    memset(bits,0,sizeof(*bits));\
+}\
+void bits##SIZE##_copy(const struct bits##SIZE *bits,struct bits##SIZE *copy){\
+    memcpy(copy,bits,sizeof(*bits));\
+}\
+void bits##SIZE##_print(const struct bits##SIZE *bits){\
+    for(unsigned i=0;i<SIZE;++i){\
+        if(i % 16 == 0){\
+            printf("%d\t",i);\
+        }\
+        else if((i % 16) % 4 == 0){\
+            putchar(' ');\
+        }\
+        putchar(bits##SIZE##_test(bits,i) ? '1' : '0');\
+        if(i % 16 == 15){\
+            putchar('\n');\
+        }\
+    }\
+    putchar('\n');\
+}\
+bool bits##SIZE##_test(const struct bits##SIZE *bits,unsigned offset){\
+    assert(offset < SIZE);\
+    return bits->data[offset/64] & uint64_set[offset%64];\
+}
 
 
 
-void uint64_reset_before(uint64_t *b,int n);
-
-void uint64_reset_after(uint64_t *b,int n);
-
-uint64_t uint64_is_subset_of_mask(
-	uint64_t set,
-	uint64_t subset
-);
+BITSET_DECLARE(235);
--- src/main.c
+++ src/main.c
@@ -9,17 +9,36 @@
 #include "main_window.h"
 #include "board.h"
 
+#include "bitset.h"
 
 
 int main(
 	int argc,
 	char **argv
 ){
-	srand(time(NULL));
+	/*srand(time(NULL));
     struct trexo_main_window window;
     gtk_init(&argc,&argv);
     trexo_main_window_init(&window);  
     //g_timeout_add(100,(GSourceFunc)trexo_game_config_timeout,&window.config);
     gtk_main();
-    return 0;  
+    */  
+    (void)argc;
+    (void)argv;
+
+
+    struct bits235 x;
+    bits235_init(&x);
+    bits235_print(&x);
+
+
+
+
+    /*
+    uint64_t b = 0xff;
+    assert(uint64_first_index(b) == 0);
+    assert(uint64_first_bit(b) == 0x1);
+    assert(uint64_count(b) == 8);
+    */
+    return 0;
 }
